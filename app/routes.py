6581d96c97152fe37d9aa5759ff87358ac6b5cf4
Martin Peck <mpeck@hotmail.co.uk>
2018-09-29 21:17:23 +0100

    use url_for

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, flash, redirect
+from flask import render_template, flash, redirect, url_for
 from app import app
 from app.forms import LoginForm
 
@@ -27,5 +27,6 @@ def login():
       form.username.data,
       form.remember_me.data
     ))
+    return redirect(url_for('index'))
 
   return render_template('login.html', title='Sign In', form=form)
