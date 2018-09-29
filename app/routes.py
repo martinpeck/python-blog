055e8be7c351d919207df1382d033b1a70452770
Martin Peck <mpeck@hotmail.co.uk>
2018-09-29 20:43:08 +0100

    add additional content to index, and add base template

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-  return "Hello, World!"
+  user = {'username': 'Martin'}
+  posts = [
+    {
+      'author': {'username': 'Martin'},
+      'body': 'My first blog post!'
+    },
+    {
+      'author': {'username': 'Jessica'},
+      'body': 'Getting a better understanding of Python'
+    }
+  ]
+  return render_template('index.html', title='Home', user=user, posts=posts)
