5b9cfd7caace04c544746c423bb1aedab630aa96
Martin Peck <mpeck@hotmail.co.uk>
2018-10-02 22:48:45 +0100

    add last_seen attribute to user

--- app/routes.py
+++ app/routes.py
@@ -4,6 +4,13 @@ from app import app, db
 from app.forms import LoginForm, RegistrationForm
 from flask_login import current_user, login_user, logout_user, login_required
 from app.models import User
+from datetime import datetime
+
+@app.before_request
+def before_request():
+  if current_user.is_authenticated:
+    current_user.last_seen = datetime.utcnow()
+    db.session.commit()
 
 @app.route('/')
 @app.route('/index')
