0a6ede0fe58bbc6af09c3ed041c2f2b494b8dfba
Martin Peck <martinjohnpeck@gmail.com>
2018-10-05 14:15:55 +0100

    integrate rollbar

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,20 @@
-from flask import render_template
+from flask import render_template, got_request_exception
 from app import app, db
+import os
+import rollbar
+import rollbar.contrib.flask
+
+@app.before_first_request
+def init_rollbar():
+  if not app.debug:
+    if os.environ.get('ROLLBAR_ACCESS_TOKEN'):
+      rollbar.init(
+            os.environ.get('ROLLBAR_ACCESS_TOKEN'),
+            'python-blog-dev',
+            root=os.path.dirname(os.path.realpath(__file__)),
+            allow_logging_basic_config=False)
+      got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+      rollbar.report_message('Rollbar configured for python-blog')
 
 @app.errorhandler(404)
 def not_found_error(error):
