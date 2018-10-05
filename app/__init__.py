ed228ec4dff1dda780470cccc7c45e3d50d7555e
Martin Peck <martinjohnpeck@gmail.com>
2018-10-05 13:25:57 +0100

    add error pages

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 
+
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
@@ -13,4 +14,4 @@ login = LoginManager(app)
 # define view for logging in
 login.login_view = 'login'
 
-from app import routes, models
+from app import routes, models, errors
