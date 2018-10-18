321dde10b42132f08995b265203972b44d303dcd
Martin Peck <mpeck@hotmail.co.uk>
2018-10-18 11:50:28 +0100

    bootstrap all the things

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_bootstrap import Bootstrap
 import logging
 from logging.handlers import RotatingFileHandler
 import os
@@ -13,6 +14,7 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+bootstrap = Bootstrap(app)
 
 # define view for logging in
 login.login_view = 'login'
